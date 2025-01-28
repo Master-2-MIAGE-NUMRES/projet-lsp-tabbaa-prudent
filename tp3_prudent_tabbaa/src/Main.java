3220f3474eb922f08c3232e5ed7cab9ce1069a74
ArthurPrdnt <arthur.prudent@etu.unice.fr>
2025-01-28 11:52:12 +0100

    ajout currentAccount

--- tp3_prudent_tabbaa/src/Main.java
+++ tp3_prudent_tabbaa/src/Main.java
@@ -1,4 +1,5 @@
 import accounts.BankAccount;
+import accounts.CurrentAccount;
 import accounts.SavingsAccount;
 
 public class Main {
@@ -14,5 +15,20 @@ public class Main {
         } catch (UnsupportedOperationException e) {
             System.out.println(e);
         }
+
+        BankAccount currentAccount = new CurrentAccount("2", 69069);
+        currentAccount.deposit(0);
+
+        System.out.println("Solde compte courant : " + currentAccount.getBalance());
+
+        currentAccount.withdraw(69069);
+        System.out.println("Solde compte courant : " + currentAccount.getBalance());
+
+        try {
+            currentAccount.withdraw(200);
+        } catch (UnsupportedOperationException e) {
+            System.out.println(e);
+        }
+
     }
 }
--- tp3_prudent_tabbaa/src/accounts/CurrentAccount.java
+++ tp3_prudent_tabbaa/src/accounts/CurrentAccount.java
@@ -1,4 +1,27 @@
 package accounts;
 
-public class CurrentAccount {
+public class CurrentAccount extends BankAccount{
+
+    public CurrentAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
+    @Override
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    @Override
+    public void withdraw(double amount) throws UnsupportedOperationException {
+        if (balance > 0) {
+            balance -= amount;
+        }
+        else {
+            throw new UnsupportedOperationException("Vous etes povre #LOSER \uD83D\uDE2D");
+        }
+    }
+
+
 }
